8f6423eb4004653d6b6b7594befe187968e55aad
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-09-25 12:09:29 -0300

    style: the selected focus now has an up arrow

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,7 +73,8 @@ export default {
         "def": "0px"
       },
       backgroundImage: {
-        "button": `url('data:image/svg+xml;utf8,<svg width="16" height="16" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 10.127L12 18.127L20 10.127H4Z" fill="%23f4f4f4"/></svg>')`
+        "arrow": `url('data:image/svg+xml;utf8,<svg width="16" height="16" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 10.127L12 18.127L20 10.127H4Z" fill="%23f4f4f4"/></svg>')`,
+        "arrowClick": `url('data:image/svg+xml;utf8,<svg width="16" height="16" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 15.873L12 7.873L20 15.873H4Z" fill="%23f4f4f4"/></svg>')`
       },
       backgroundPosition: {
         "right-4": "bottom 0.9rem right 0.5rem"
