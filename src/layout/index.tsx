64de05f22d1c47c260daa28fffd2098c5492dc92
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2023-12-24 21:54:39 -0300

    refactor: remove the Footer component

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,24 +1,20 @@
 import { CountryContext } from '../contexts/country.context'
 import { useContext, PropsWithChildren } from 'react'
 import Header from '../components/Header'
-import Footer from '../components/Footer'
 
 const Layout = ({ children }: PropsWithChildren) => {
   const { setOpenModal } = useContext(CountryContext)
 
   return (
-    <>
-      <div className="min-h-screen w-full px-4 pb-7 pt-4">
-        <Header />
-        <main
-          onClick={() => setOpenModal(false)}
-          className="m-auto w-full max-w-1300"
-        >
-          {children}
-        </main>
-      </div>
-      <Footer />
-    </>
+    <div className="min-h-screen w-full px-4 pb-7 pt-4">
+      <Header />
+      <main
+        onClick={() => setOpenModal(false)}
+        className="m-auto w-full max-w-1300"
+      >
+        {children}
+      </main>
+    </div>
   )
 }
 
