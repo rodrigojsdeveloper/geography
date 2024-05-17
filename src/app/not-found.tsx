587d5a294b0367361867c5d66d4b3a2c97a5a3f0
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-05-17 19:40:37 -0300

    style: add w-full

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <main className="mx-auto flex max-w-[41.25rem] flex-col gap-y-8 px-4 pt-24">
+    <main className="mx-auto flex w-full max-w-[41.25rem] flex-col gap-y-8 px-4 pt-24">
       <h1 className="text-primary text-5xl font-semibold leading-none">404</h1>
       <div className="flex flex-col gap-y-2">
         <h2>Page is not found</h2>
