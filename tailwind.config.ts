0f25248dc8c85b5e5dcb516dc59939e0ebf2c026
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-05-18 19:29:45 -0300

    style: adjust global style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['--font-poppins'],
+        poppins: ['var(--font-poppins)'],
       },
       colors: {
         green: {
